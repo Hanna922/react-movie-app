aba9465079d31d4c796e286665eefe3b1e0ca736
Hanna922 <hanna.urssu@gmail.com>
2023-01-06 22:07:34 +0900

    ~#7.6 react movie app

--- src/App.js
+++ src/App.js
@@ -1,49 +1,25 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]);
-  };
-  const deleteBtn = (index) => {
-    setToDos((currentToDos) =>
-      currentToDos.filter((_, currentIndex) => index !== currentIndex)
-    );
-  };
   return (
-    <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>
-            {item} <button onClick={() => deleteBtn(index)}>x</button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/abot-us">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
 
-// onClick={deleteBtn}이 아닌 onClick={() => deleteBtn(index)} 라고 쓰는 이유는 ?
-// => index 값을 넘기기 위한 것
+// npm i react-router-dom@5.3.0 버전에서의 코드
---
+++ src/components/Movie.js
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+function Movie({ id, coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} alt={title} />
+      <h2>
+        <h2>
+          {/* <a href="/movie">{title}</a> 를 사용하면 페이지 전체를 재실행하게 됨,
+              반면 <Link 사용 시 새로고침하지 않음! */}
+          <Link to={`/movie/${id}`}>{title}</Link>
+        </h2>
+      </h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;

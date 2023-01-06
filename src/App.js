add794b9bf7f8f4b44f235da66a5c5e5cf3742d3
Hanna922 <hanna.urssu@gmail.com>
2023-01-06 18:58:55 +0900

    ~#7.1 react To Do List Part Two

--- src/App.js
+++ src/App.js
@@ -2,30 +2,48 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function Hello() {
-  // useEffect(function () {
-  //   console.log("hi :)");
-  //   return function () {
-  //     console.log("bye :(");
-  //   };
-  // }, []);
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-    // => component가 destory 될 때 실행 될 function = Cleanup function
-  }, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
+  const deleteBtn = (index) => {
+    setToDos((currentToDos) =>
+      currentToDos.filter((_, currentIndex) => index !== currentIndex)
+    );
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>
+            {item} <button onClick={() => deleteBtn(index)}>x</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
+// onClick={deleteBtn}이 아닌 onClick={() => deleteBtn(index)} 라고 쓰는 이유는 ?
+// => index 값을 넘기기 위한 것

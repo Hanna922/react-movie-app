cfe4f209bec6e443bd21a77890837f1076ebfc1d
Hanna922 <hanna.urssu@gmail.com>
2023-01-06 22:34:31 +0900

    #7 practice movie app

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,23 +1,25 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
-function Movie({ id, coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, year, summary, genres }) {
   return (
-    <div>
-      <img src={coverImg} alt={title} />
-      <h2>
-        <h2>
+    <div className={styles.movie}>
+      <img src={coverImg} alt={title} className={styles.movie__img} />
+      <div>
+        <h2 className={styles.movie__title}>
           {/* <a href="/movie">{title}</a> 를 사용하면 페이지 전체를 재실행하게 됨,
               반면 <Link 사용 시 새로고침하지 않음! */}
           <Link to={`/movie/${id}`}>{title}</Link>
         </h2>
-      </h2>
-      <p>{summary}</p>
-      <ul>
-        {genres.map((g) => (
-          <li key={g}>{g}</li>
-        ))}
-      </ul>
+        <h3 className={styles.movie__year}>{year}</h3>
+        <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+        <ul className={styles.movie__genres}>
+          {genres.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
